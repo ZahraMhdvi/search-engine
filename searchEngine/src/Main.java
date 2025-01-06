2cba357c7c12c219130e2c92c84b57381a07dc59
Zahra <Zmsmart2020@gmail.com>
2025-01-06 11:01:44 +0330

    convert files to .txt files to read them and edit main

--- searchEngine/src/Main.java
+++ searchEngine/src/Main.java
@@ -1,5 +1,31 @@
+
+import SearchEngine.SearchEngine;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" world!");
+        Scanner scanner = new Scanner(System.in);
+        SearchEngine searchEngine = new SearchEngine();
+
+        try {
+            searchEngine.preprocessDocuments("C:\\Users\\DataStructure\\EnglishData");
+        } catch (Exception e) {
+            System.out.println("Error processing files: " + e.getMessage());
+            return;
+        }
+
+        System.out.println("Enter the number of queries:");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter query " + (i + 1) + ":");
+            String query = scanner.nextLine();
+
+            List<String> results = searchEngine.search(query);
+            searchEngine.printSearchResults(results);
+        }
+
     }
 }
